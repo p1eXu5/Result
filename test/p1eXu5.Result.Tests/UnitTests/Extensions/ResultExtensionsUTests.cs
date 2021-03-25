961d2e15be39ebabf278283d02b07ae1625da042
p1eXu5 <v1adp1exu5@gmail.com>
2021-03-25 14:15:01 +0300

    add tests

---
+++ test/p1eXu5.Result.Tests/UnitTests/Extensions/ResultExtensionsUTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using p1eXu5.Result.Extensions;
+using p1eXu5.Result.Generic;
+using Shouldly;
+
+namespace p1eXu5.Result.Tests.UnitTests.Extensions
+{
+    public class ResultExtensionsUTests
+    {
+        [Test]
+        public void Bind_FunctionReturnsSucceeded_ReturnsSucceededWithExpectedType()
+        {
+            // Arrange:
+            Result<int> v = Result.Success(18);
+            Func<int, Result<string>> f = i => Result.Success(i.ToString());
+
+            // Action:
+            var actual = v.Bind( f );
+
+            // Assert:
+            actual.ShouldBe( Result.Success("18") );
+        }
+
+        [Test]
+        public void Bind_FunctionReturnsFailure_ReturnsFailureWithExpectedType()
+        {
+            // Arrange:
+            Result<int> v = Result.Success(18);
+            Func<int, Result<Type>> f = i => Result.Failure<Type>(i.ToString());
+
+            // Action:
+            var actual = v.Bind( f );
+
+            // Assert:
+            actual.ShouldBe( Result.Failure<Type>("18") );
+        }
+    }
+}
