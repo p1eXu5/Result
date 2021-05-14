2e89e21de6a0a90a87b6830bd8da17745591b284
v1adp1exu5@gmail.com <v1adp1exu5@gmail.com>
2021-05-14 17:27:56 +0300

    [ chore, ref ] - add ToTaskResult extension methods, fix warning

--- src/p1eXu5.Result/Extensions/TaskExtensions.cs
+++ src/p1eXu5.Result/Extensions/TaskExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using p1eXu5.Result.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace p1eXu5.Result.Extensions
@@ -17,6 +19,53 @@ namespace p1eXu5.Result.Extensions
         {
             var aResult = await task;
             return f( aResult );
+        }
+
+
+
+
+        public static async Task< Result< TContext >> ToTaskResult< TContext >( this Task<TContext> task, CancellationToken cancellationToken )
+        {
+            if ( task.IsCanceled ) {
+                return Result< TContext >.Failure( "Task was canceled." );
+            }
+
+            if ( task.IsFaulted ) {
+                return Result< TContext >.Failure( task.Exception!.Flatten() );
+            }
+
+            try {
+                var result = await task;
+                return Result< TContext >.Success( result );
+            }
+            catch (Exception ex) {
+                return Result< TContext >.Failure( ex );
+            }
+        }
+
+        public static async Task< Result> ToTaskResult( this Task task, CancellationToken cancellationToken )
+        {
+            if ( task.IsCanceled ) {
+                return Result.Failure( "Task was canceled." );
+            }
+
+            if ( task.IsFaulted ) {
+                return Result.Failure( task.Exception!.Flatten() );
+            }
+
+            return await task.ContinueWith( 
+                t => {
+                    if (t.IsCompleted) {
+
+                        return Result.Success();
+                    }
+                    else if (t.IsFaulted) {
+                        return Result.Failure( t.Exception!.Flatten() );
+                    }
+                    else {
+                        return Result.Failure( "Task was canceled." );
+                    }
+                }, cancellationToken);
         }
     }
 }
